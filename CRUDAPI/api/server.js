c07e7ee020d3f70682b43e35ab439977eefde8cc
Traven Wells <104939327+WellsTraven-FS@users.noreply.github.com>
2023-01-20 14:43:06 -0500

    api route changes

--- CRUDAPI/api/server.js
+++ CRUDAPI/api/server.js
@@ -19,7 +19,15 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Database Connection Established"));
 
 app.use(express.json());
-app.use("/students", studentRouter);
+app.use("/api/v1/students", studentRouter);
+
+// look in the react build folder for static build
+app.use(express.static(path.join(__dirname, "../reactjs/build")));
+
+// for any routes not defined by the api, assume it's a direct request to a client-side route
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../reactjs/build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
